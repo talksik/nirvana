1a2265e70f44f410bae06fa27dc6437521c5a452
Arjun Patel <patel.arjun50@gmail.com>
2022-01-14 21:37:33 -0800

    fix lint problem for now as not using api

--- pages/api/auth/validateToken.tsx
+++ pages/api/auth/validateToken.tsx
@@ -1,39 +1,37 @@
+// import fbAdm, { credential } from 'firebase-admin';
+// import firebaseAdmin, { App } from 'firebase-admin/app';
 
-import fbAdm, { credential } from 'firebase-admin';
-import firebaseAdmin, { App } from 'firebase-admin/app';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-import { NextApiRequest, NextApiResponse } from 'next';
+// import serviceAccount from '../../../nirvana-for-business-firebase-adminsdk'
 
-import serviceAccount from '../../../nirvana-for-business-firebase-adminsdk'
+// const adminApp = firebaseAdmin.initializeApp({
+//   credential: credential.cert({
+//     privateKey: serviceAccount.private_key,
+//     clientEmail: serviceAccount.client_email,
+//     projectId: serviceAccount.project_id,
+//   })
+// });
 
-const adminApp = firebaseAdmin.initializeApp({
-  credential: credential.cert({
-    privateKey: serviceAccount.private_key,
-    clientEmail: serviceAccount.client_email,
-    projectId: serviceAccount.project_id,
-  })
-});
+// export default async function(req: NextApiRequest, res: NextApiResponse) {
+//   console.log('Validating token...');
 
+//   try {
+//     const { token } = JSON.parse(req.headers.authorization || '{}');
+//     if (!token) {
+//       return res.status(403).send({
+//         errorCode: 403,
+//         message: 'Auth token missing.'
+//       });
+//     }
 
-export default async function(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Validating token...');
-  
-  try {
-    const { token } = JSON.parse(req.headers.authorization || '{}');
-    if (!token) {
-      return res.status(403).send({
-        errorCode: 403,
-        message: 'Auth token missing.'
-      });
-    }
+//     const verifiedToken = fbAdm.auth().verifyIdToken(token)
 
-    const verifiedToken = fbAdm.auth().verifyIdToken(token)
-
-    return res.status(200).send(verifiedToken);
-  } catch (err) {
-    return res.status(err.code).send({
-      errorCode: err.code,
-      message: err.message,
-    });
-  }
-}
+//     return res.status(200).send(verifiedToken);
+//   } catch (err) {
+//     return res.status(err.code).send({
+//       errorCode: err.code,
+//       message: err.message,
+//     });
+//   }
+// }
