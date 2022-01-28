fd19b0f1bfe44ea930f18aa1072c6666913f9ccb
Arjun Patel <patel.arjun50@gmail.com>
2022-01-27 20:37:04 -0800

    setting up sendgrid and stuff

--- serverless/functions/src/notifications.ts
+++ serverless/functions/src/notifications.ts
@@ -1,11 +1,14 @@
 // import * as functions from "firebase-functions";
+import * as sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(
+  "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"
+);
 
 // exports.inviteUserToTeam = functions.firestore
 //     .document('teamMembers/{userId}')
-//     .onCreate((snap, context) => {
-//       // Get an object representing the document
-//       // e.g. {'name': 'Marie', 'age': 66}
-//       const newValue = snap.data();
+//     .onCreate(async (snap, context) => {
+//       const newTeamMember = snap.data() as TeamMember;
 
 //       // access a particular field as you would any JS property
 //       const name = newValue.name;
